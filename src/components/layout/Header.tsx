32181057407112de6421b021f455a2b66a49cae8
Avani Bataviya <avanibataviya2610@gmail.com>
2022-12-12 13:15:40 +0530

    added products as menu in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,15 +1,37 @@
 import { ShoppingCart } from "@mui/icons-material";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import styles from "../../../styles/Home.module.css";
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <ShoppingCart sx={{ mr: 2 }} />
-        <Typography variant="h6" color="inherit" noWrap>
-          Ecommerce app
-        </Typography>
+        <Box
+          sx={{
+            cursor: "pointer",
+            flexGrow: 1,
+            display: "flex",
+            alignItems: "center",
+          }}
+          onClick={() => router.push("/")}
+        >
+          <ShoppingCart sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Ecommerce app
+          </Typography>
+        </Box>
+        {true && (
+          <>
+            <Link href="/products" className={styles.link}>
+              <Button color="inherit">Products</Button>
+            </Link>
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
